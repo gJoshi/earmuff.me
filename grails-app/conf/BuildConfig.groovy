0ea3a6c8095f870a953bc40817cf28e462a9ba70
dbpatel219 <dbpatel219@gmail.com>
2014-04-19 00:21:57 -0400

    Initial earmuffs repository

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -2,8 +2,8 @@ grails.servlet.version = "2.5" // Change depending on target container complianc
 grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
-grails.project.target.level = 1.6
-grails.project.source.level = 1.6
+grails.project.target.level = 1.7
+grails.project.source.level = 1.7
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
 
 // uncomment (and adjust settings) to fork the JVM to isolate classpaths
@@ -12,53 +12,57 @@ grails.project.source.level = 1.6
 //]
 
 grails.project.dependency.resolution = {
-    // inherit Grails' default dependencies
-    inherits("global") {
-        // specify dependency exclusions here; for example, uncomment this to disable ehcache:
-        // excludes 'ehcache'
-    }
-    log "error" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
-    checksums true // Whether to verify checksums on resolve
-    legacyResolve false // whether to do a secondary resolve on plugin installation, not advised and here for backwards compatibility
+	// inherit Grails' default dependencies
+	inherits("global") {
+		// specify dependency exclusions here; for example, uncomment this to disable ehcache:
+		// excludes 'ehcache'
+	}
+	log "error" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
+	checksums true // Whether to verify checksums on resolve
+	legacyResolve false // whether to do a secondary resolve on plugin installation, not advised and here for backwards compatibility
 
-    repositories {
-        inherits true // Whether to inherit repository definitions from plugins
+	repositories {
+		inherits true // Whether to inherit repository definitions from plugins
 
-        grailsPlugins()
-        grailsHome()
-        grailsCentral()
+		grailsPlugins()
+		grailsHome()
+		grailsCentral()
 
-        mavenLocal()
-        mavenCentral()
+		mavenLocal()
+		mavenCentral()
 
-        // uncomment these (or add new ones) to enable remote dependency resolution from public Maven repositories
-        //mavenRepo "http://snapshots.repository.codehaus.org"
-        //mavenRepo "http://repository.codehaus.org"
-        //mavenRepo "http://download.java.net/maven/2/"
-        //mavenRepo "http://repository.jboss.com/maven2/"
-    }
+		// uncomment these (or add new ones) to enable remote dependency resolution from public Maven repositories
+		//mavenRepo "http://snapshots.repository.codehaus.org"
+		//mavenRepo "http://repository.codehaus.org"
+		//mavenRepo "http://download.java.net/maven/2/"
+		//mavenRepo "http://repository.jboss.com/maven2/"
+	}
 
-    dependencies {
-        // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes e.g.
+	dependencies {
+		// specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes e.g.
+		compile 'com.moviejukebox:thetvdbapi:1.4'
+		compile 'org.codehaus.groovy.modules.http-builder:http-builder:0.5.2' {
+			excludes 'groovy'
+		}
 
-        // runtime 'mysql:mysql-connector-java:5.1.20'
-        runtime 'postgresql:postgresql:8.4-702.jdbc3'
-    }
+		runtime 'postgresql:postgresql:9.0-801.jdbc4'
+	}
 
-    plugins {
-        runtime ":hibernate:$grailsVersion"
-        runtime ":jquery:1.8.3"
-        runtime ":resources:1.1.6"
+	plugins {
+		runtime ":hibernate4:4.3.4.1"
+		compile ":jquery:1.11.0.2"
+		runtime ":resources:1.2"
 
-        // Uncomment these (or add new ones) to enable additional resources capabilities
-        //runtime ":zipped-resources:1.0"
-        //runtime ":cached-resources:1.0"
-        //runtime ":yui-minify-resources:0.1.4"
+		// Uncomment these (or add new ones) to enable additional resources capabilities
+		//runtime ":zipped-resources:1.0"
+		//runtime ":cached-resources:1.0"
+		//runtime ":yui-minify-resources:0.1.5"
 
-        build ":tomcat:$grailsVersion"
-
-        runtime ":database-migration:1.2.1"
-
-        compile ':cache:1.0.1'
-    }
+		build ":tomcat:7.0.52.1"
+		runtime ":database-migration:1.3.8"
+		compile ':cache:1.1.1'
+		
+		compile ":spring-security-core:1.2.7.3"
+		compile ":rest-client-builder:2.0.1"
+	}
 }
